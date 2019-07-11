1f7ab8437cc3fea0163acbc8718c488218e0b4a3
wangweiguang <wwg2008123@163.com>
2019-07-11 16:37:49 +0800

    firest

---
+++ src/main/java/com/wwg/dockereurekaservice/DockerEurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.wwg.dockereurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DockerEurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerEurekaServiceApplication.class, args);
+    }
+
+}
